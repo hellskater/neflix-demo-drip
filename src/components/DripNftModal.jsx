cbbb87440d61b28501a435be75f9902fe9c0cd6e
hellskater <ksrinivasrao531@gmail.com>
2022-10-04 16:28:54 +0530

    modal text size changed

--- src/components/DripNftModal.jsx
+++ src/components/DripNftModal.jsx
@@ -3,7 +3,7 @@ import Modal from "../CustomUi/CustomModal";
 const DripNftModal = ({ isOpen, onClose }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} size="w-[80%] h-fit" centered>
-      <main className="w-full flex items-center justify-center text-6xl py-14 px-7 text-white">
+      <main className="w-full flex items-center justify-center text-5xl py-14 px-7 text-white text-center 2xl:text-6xl">
         Access granted to only DripNFT holders
       </main>
     </Modal>
